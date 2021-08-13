d3a091837b06a69a80ed960241b7d3522ea987a7
Pedro Ferreira <pedroferreiracjr@gmail.com>
2021-08-13 15:29:54 -0300

    implementação de filtro de requisição para monitorar o tempo de execução da aplicação

---
+++ src/main/java/br/com/dotofcodex/alura_servlets/filter/MonitoramentoFilter.java
@@ -0,0 +1,25 @@
+package br.com.dotofcodex.alura_servlets.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter(urlPatterns = { "/entrada" })
+public class MonitoramentoFilter implements Filter {
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		String action = request.getParameter("action");
+
+		long before = System.currentTimeMillis();
+		chain.doFilter(request, response);
+		long after = System.currentTimeMillis();
+
+		System.out.println("Tempo de execução da ação: " + action + " -> " + (after - before));
+	}
+}
